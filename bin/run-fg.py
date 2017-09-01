9d6691ea1957cdb6b9d182d4c646d98b2b5fc357
Peter Milne <peter.milne@d-tacq.com>
2017-09-01 17:52:02 +0100

    prefer CLKDIV to keep the UI up to date 	modified:   bin/run-fg.py

--- bin/run-fg.py
+++ bin/run-fg.py
@@ -16,7 +16,7 @@ uut = acq400_hapi.Acq400(args.uut[0])
 uut.load_awg(args.awg, autorearm=True)
 uut.s1.trg = '1,1,1'
 uut.s1.clk = '1,0,0'
-uut.s1.clkdiv = 2       # 2MHz/2 = 1MSPS
+uut.s1.CLKDIV = 2       # 2MHz/2 = 1MSPS
 uut.s0.SIG_SRC_TRG_1 = 'GPG1'
 uut.s0.SIG_SRC_CLK_0 = 'HDMI'
 uut.s0.SIG_SRC_TRG_0 = 'HDMI'
