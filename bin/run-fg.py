90951278fb19a67c97e4b105c3847bbe0d9d64ae
Peter Milne <peter.milne@d-tacq.com>
2017-09-03 12:01:04 +0100

    python awg load fails .. use nc. WORKTODO: how does nc reach the parts that python does not?. Guessing: nc both WRITES and READS the socket. 	modified:   bin/run-fg 	modified:   bin/run-fg.py

--- bin/run-fg.py
+++ bin/run-fg.py
@@ -7,16 +7,18 @@ import sys
 
 
 parser = argparse.ArgumentParser(description='run-fg: runs the function gen')
-parser.add_argument('--awg', help="awg raw data file")
+parser.add_argument('--awg', default="none", help="awg raw data file")
 parser.add_argument('uut', nargs='+', help="uut")
 
 args = parser.parse_args()
 uut = acq400_hapi.Acq400(args.uut[0])
 
-uut.load_awg(args.awg, autorearm=True)
+#if args.awg != "none":
+#	uut.load_awg(args.awg, autorearm=True)
+
 uut.s1.trg = '1,1,1'
 uut.s1.clk = '1,0,0'
-uut.s1.CLKDIV = 2       # 2MHz/2 = 1MSPS
+uut.s1.clkdiv = 2       # 2MHz/2 = 1MSPS
 uut.s0.SIG_SRC_TRG_1 = 'GPG1'
 uut.s0.SIG_SRC_CLK_0 = 'HDMI'
 uut.s0.SIG_SRC_TRG_0 = 'HDMI'
