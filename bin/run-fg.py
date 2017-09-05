532b9e2c0c6ff4e06b839f9313a3182aff92e351
Peter Milne <peter.milne@d-tacq.com>
2017-09-05 14:40:28 +0100

    set CLK PV's as well as clk=1,0,1 .. could be a race 	modified:   bin/run-fg.py

--- bin/run-fg.py
+++ bin/run-fg.py
@@ -18,6 +18,10 @@ uut = acq400_hapi.Acq400(args.uut[0])
 
 uut.s1.trg = '1,1,1'                # AWG TRG d1
 uut.s1.clk = '1,0,0'                # AWG CLK d0
+# do we have a race condition here?
+# make sure the PV's agree ..
+uut.s1.CLK = '1'
+uut.s1.CLK_DX = 'd0'
 uut.s1.CLKDIV = '2'                 # 2MHz/2 = 1MSPS
 uut.s0.SIG_EVENT_SRC_1 = 'GPG'      # front panel output for monitor.
 uut.s0.SIG_SRC_TRG_1 = 'GPG1'       # AWG trigger is GPG1 (internal)
