b48637e8de3581658c35a429a1a0f9232607d642
Peter Milne <peter.milne@d-tacq.com>
2017-09-05 13:42:47 +0100

    prefer CLKDIV for display clarity. 	modified:   bin/run-fg.py

--- bin/run-fg.py
+++ bin/run-fg.py
@@ -16,12 +16,12 @@ uut = acq400_hapi.Acq400(args.uut[0])
 #if args.awg != "none":
 #	uut.load_awg(args.awg, autorearm=True)
 
-uut.s1.trg = '1,1,1'
-uut.s1.clk = '1,0,0'
-uut.s1.clkdiv = 2       # 2MHz/2 = 1MSPS
+uut.s1.trg = '1,1,1'                # AWG TRG d1
+uut.s1.clk = '1,0,0'                # AWG CLK d0
+uut.s1.CLKDIV = 2                   # 2MHz/2 = 1MSPS
 uut.s0.SIG_EVENT_SRC_1 = 'GPG'      # front panel output for monitor.
-uut.s0.SIG_SRC_TRG_1 = 'GPG1'
-uut.s0.SIG_SRC_CLK_0 = 'HDMI'
-uut.s0.SIG_SRC_TRG_0 = 'HDMI'
+uut.s0.SIG_SRC_TRG_1 = 'GPG1'       # AWG trigger is GPG1 (internal)
+uut.s0.SIG_SRC_CLK_0 = 'HDMI'       # CLK is SLAVED
+uut.s0.SIG_SRC_TRG_0 = 'HDMI'       # TRG is SLAVED
 
 
